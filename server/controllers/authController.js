818a0b65e1091abeb8f28192b88a69e87086922b
corey clarke <sombas100@yahoo.com>
2024-04-24 20:16:50 +0100

    Create login, register and logout functionalities

---
+++ server/controllers/authController.js
@@ -0,0 +1,59 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
+const registerUser = async (req, res) => {
+    const { username, password } = req.body
+
+    try {
+        let user = await User.findOne({ username });
+
+        if(user) {
+            return res.status(400).json({ message: 'User already exists' })
+        }
+
+        user = new User({
+            username,
+            password,
+        })
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt)
+
+        await user.save()
+
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json({ message: 'Internal Server Error'})
+    }
+}
+
+const loginUser = async (req, res) => {
+    const { username, email, password} = req.body
+
+    try {
+        let user = await User.findOne({ email })
+        if(!user) {
+            return res.status(400).json({ message: 'Invalid Credentials '})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid Credentials'})
+        }
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json({ message: 'Internal Server Error'})
+    }
+}
+
+
+const logoutUser = async (req, res) => {
+
+}
+
+module.exports = {
+    registerUser, loginUser, logoutUser
+}
+
