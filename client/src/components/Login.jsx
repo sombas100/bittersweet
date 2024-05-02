a8f7ccae6ecad5872a8deb7e85ada57dbadb859b
corey clarke <sombas100@yahoo.com>
2024-05-02 22:15:27 +0100

    Style the login and register pages

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import axios from 'axios'
 import { useState, } from 'react'
 import { useNavigate } from 'react-router-dom'
+import '../App.css'
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom'
 
 const Login = ({ setAuthenticated }) => {
   const navigate = useNavigate()
@@ -34,33 +37,45 @@ try {
   }
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input 
-          type='text'
-          placeholder='Email'
-          name='email'
-          value={email}
-          onChange={handleChange}
-          required
-          />
-        </div>
-        <div>
-          <input 
-          type='password'
-          placeholder='Password'
-          name='password'
-          value={password}
-          onChange={handleChange}
-          required
-          />
-        </div>
-        <button type='submit'>Login</button>
-      </form>
+    <div className="login-container">
+      <h1 className='login-logo'>Bitter<span className='sweet'>sweet</span></h1>
+      <p className='motto'>A place to share</p>
+      <div className="login-header">
+        <h2>Sign in</h2>
+        <p>New to Bittersweet?<Link style={{ textDecoration: 'none'}} 
+        to='/register'><span className="shortcut">Create an account here.</span>
+        </Link></p>
+      </div>
+      <div className="login-actions">
+        
+        <form onSubmit={handleSubmit} className="login-form">
+          <div className="form-input">
+            
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-input">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <Button type='submit' size='small' variant="contained">Log In</Button>
+          <footer><span className='copyright'>© bittersweet 2024</span></footer>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios'
+import { Link } from 'react-router-dom'
+import Button from '@mui/material/Button';
 
 const Register = () => {
   const navigate = useNavigate()
@@ -32,43 +34,53 @@ const handleSubmit = async e => {
     console.error('Error during registration', error.response.data)
   }}
   return (
-    <div>
-      <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input 
-          type='text'
-          placeholder='Username'
-          name='username'
-          value={username}
-          onChange={handleChange}
-          required
-          />
-        </div>
-        <div>
-          <input 
-          type='email'
-          placeholder='Email'
-          name='email'
-          value={email}
-          onChange={handleChange}
-          required
-          />
-        </div>
-        <div>
-          <input 
-          type='password'
-          placeholder='Password'
-          name='password'
-          value={password}
-          onChange={handleChange}
-          required
-          />
-        </div>
-        <button type='submit'>Submit</button>
-      </form>
+    <div className="login-container">
+      <h1 className='login-logo'>Bitter<span className='sweet'>sweet</span></h1>
+      <p className='motto'>A place to share</p>
+      <div className="login-header">
+        <h2>Create Account</h2>
+        <p>Already have an account?<Link style={{ textDecoration: 'none'}} 
+        to='/login'><span className="shortcut">Sign in here.</span>
+        </Link></p>
+      </div>
+      <div className="login-actions">
+        
+        <form onSubmit={handleSubmit} className="login-form">
+          <div className="form-input">
+            <input
+              type="username"
+              placeholder="Username"
+              name="username"
+              value={username}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-input">
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-input">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <Button type='submit' size='small' variant="contained">Register</Button>
+          <footer><span className='copyright'>© bittersweet 2024</span></footer>
+        </form>
+      </div>
     </div>
   )
-
 }
 export default Register
